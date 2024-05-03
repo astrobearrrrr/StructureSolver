639d72710d8c3c60f1af958d84353cf49410f7b2
astrobearrrrr <895208821@qq.com>
2024-05-03 23:32:52 +0800

    add_some_function

--- BeamStrucutre.f90
+++ BeamStrucutre.f90
@@ -10,8 +10,9 @@ module BeamStrucutre
     type :: Segment
         private
         integer :: m_localToGlobal(1:nElmtDofs)
-        real(8) :: x0(1:nElmtDofs),x1(1:nElmtDofs), len0, bc(1:nElmtDofs), geoFRM
+        real(8) :: x0(1:nElmtDofs),x1(1:nElmtDofs), len0, len1, bc(1:nElmtDofs), geoFRM
         real(8) :: triad_ee(3,3),triad_n1(3,3),triad_n2(3,3)
+        real(8) :: forceb(1:12)
         real(8) :: m_property(1:8)
         real(8) :: m_coefMat(1:nElmtDofs, 1:nElmtDofs)
         real(8) :: m_tanMat(1:nElmtDofs, 1:nElmtDofs)
@@ -30,8 +31,13 @@ module BeamStrucutre
         procedure :: DampMatrixPlusDsp => Segment_DampMatrixPlusDsp
         procedure :: RotateMatrix => Segment_RotateMatrix
         procedure :: BoundaryCond => Segment_BoundaryCond
+        procedure :: BodyStress => Segment_BodyStress_D
+        procedure :: LoadInternal => Segment_LoadInternal
+        procedure :: UpdateTriad_D => Segment_UpdateTriad_D
+        procedure :: MakeTriad_ee => Segment_MakeTriad_ee
     end type Segment
     type(Segment), allocatable :: m_elements(:)
+    real(8), allocatable ::lodInte(:)
 
   contains
     subroutine Beam_initialise(filename, Newmarkgamma, Newmarkbeta, dt, damp1, damp2, gamma1, alphaf1)
@@ -62,6 +68,7 @@ module BeamStrucutre
             read(buffer, *) m_npts, m_nelmts, m_nmaterials
         close(fileiD)
         g_ndofs = m_npts * 6
+        allocate(lodInte(1:g_ndofs))
         ! load points data
         allocate(xyz(1:3, 1:m_npts))
         call Beam_ReadPoints(xyz)
@@ -105,6 +112,9 @@ module BeamStrucutre
             call Beam_UpdateBoundaryCond
             call Beam_UpdateRHS(rhs)
             do j = 1, maxCG
+                lodInte = 0.0
+                call BodyStress
+                call LoadInternal
                 call CG_Solve(dispn, rhs)
                 if(testconvergence) exit
             enddo
@@ -465,9 +475,16 @@ module BeamStrucutre
         implicit none
         class(Segment), intent(inout) :: this
         real(8):: rt(3,3),r(3,3),ktemp(12,12)
-        real(8):: l,m,n,beta,pi,sb,cb,d,Invd
+        real(8):: dx0,dy0,dz0,l,m,n,beta,pi,sb,cb,d,Invd
         integer:: i,j,k,j1,j2,ii,jj,in,jn
-        
+
+        dx0   = this%x0(1) - this%x0(7)
+        dy0   = this%x0(2) - this%x0(8)
+        dz0   = this%x0(3) - this%x0(9)
+        l=dx0/this%len0
+        m=dy0/this%len0
+        n=dz0/this%len0
+
         beta = this%m_property(5)
 
         pi=4.0*datan(1.0d0)
@@ -552,7 +569,7 @@ module BeamStrucutre
         ! add load boundary condition
     end subroutine Segment_BoundaryCond
 
-    subroutine init_triad_D(this)
+    subroutine Segment_InitTriad_D(this)
         implicit none
         class(Segment), intent(inout) :: this
         real(8):: dx,dy,dz
@@ -589,7 +606,298 @@ module BeamStrucutre
         ! all element triads have same initial orientation
         this%triad_n2(1:3,1:3)=this%triad_n1(1:3,1:3)
         this%triad_ee(1:3,1:3)=this%triad_n1(1:3,1:3)
-    end subroutine init_triad_D
+    end subroutine Segment_InitTriad_D
+
+    subroutine Segment_BodyStress_D(this)
+        implicit none
+        class(Segment), intent(inout) :: this
+        real(8) :: geoFRM
+        real(8) :: triad_00(3,3),triad_11(3,3),triad_22(3,3)
+        real(8) :: ub(12),dl
+        real(8) :: dx0,dy0,dz0,dx1,dy1,dz1,du,dv,dw
+        real(8) :: tx1,tx2,ty1,ty2,tz1,tz2,tx,ty,tz
+        real(8) :: fxx,emod,area
+        integer :: i,j
+
+                dx0   = this%x0(1) - this%x0(7)
+                dy0   = this%x0(2) - this%x0(8)
+                dz0   = this%x0(3) - this%x0(9)
+
+                !Define the xyz initial directional distance and the initial beam length, xl0, for the two end nodes of the beam unit
+                ! orientation
+                dx1   = this%x1(1) - this%x1(7)
+                dy1   = this%x1(2) - this%x1(8)
+                dz1   = this%x1(3) - this%x1(9)
+                ! Define the axial strain in the xyz direction (difference between displacements at two nodes)
+                du = dx1 - dx0
+                dv = dy1 - dy0
+                dw = dz1 - dz0
+
+                ! Define the the xyz directional distance of the two nodes of the beam element after deformation
+                ! as well as the length, xl, of the element after deformation
+                dl = ( (dx0+dx1)*du +(dy0+dy1)*dv +(dz0+dz1)*dw )/ (this%len0+this%len1)
+                ! get twisting angles
+                do    i=1,3
+                do    j=1,3
+                    triad_00(i,j)=this%triad_ee(i,j)
+                    triad_11(i,j)=this%triad_ee(i,j)
+                    triad_22(i,j)=this%triad_n1(i,j)
+                enddo
+                enddo
+                call Segment_get_angle_triad(triad_11,triad_22,tx,ty,tz)
+                call Segment_global_to_local(triad_00,tx,ty,tz,tx1,ty1,tz1)
+                !
+                do    i=1,3
+                do    j=1,3
+                    triad_11(i,j)=this%triad_ee(i,j)
+                    triad_22(i,j)=this%triad_n2(i,j)
+                enddo
+                enddo
+                call Segment_get_angle_triad(triad_11,triad_22,tx,ty,tz)
+                call Segment_global_to_local(triad_00,tx,ty,tz,tx2,ty2,tz2)
+
+                ! non-zero ty1 tz1 u2 tx2 ty2 tz2
+                ub(1)=0.0
+                ub(2)=0.0
+                ub(3)=0.0
+                ub(4)=tx1
+                ub(5)=ty1
+                ub(6)=tz1
+                !
+                ub(7)=dl
+                ub(8)=0.0
+                ub(9)=0.0
+                ub(10)=tx2
+                ub(11)=ty2
+                ub(12)=tz2
+                !
+                ! compute axial force
+                emod = this%m_property(1)
+                area = this%m_property(3)
+                fxx=dl*emod*area/this%len0
+                ! save local force for geo stiff
+                ! write(igeoFRM,'(D25.15)') fxx
+                geoFRM=fxx
+                ! nodal forces in local coords
+                ! {F}=[k]{u}
+                this%forceb(1:12) =matmul(this%m_stfMat,ub)
+        return
+    end subroutine Segment_BodyStress_D
+
+    subroutine Segment_LoadInternal(this)
+        implicit none
+        class(Segment), intent(inout) :: this
+        integer :: i,j,n
+        real(8) :: force(12)
+        do n = 1, m_nelmts
+            do  i=1,12
+                force(i)=0.0
+            enddo
+            do    i=1,3
+                do    j=1,3
+                    force(0+i) = force(0+i) + this%triad_ee(i,j)*this%forceb(0+j)
+                    force(3+i) = force(3+i) + this%triad_ee(i,j)*this%forceb(3+j)
+                    force(6+i) = force(6+i) + this%triad_ee(i,j)*this%forceb(6+j)
+                    force(9+i) = force(9+i) + this%triad_ee(i,j)*this%forceb(9+j)
+                enddo
+            enddo
+            do i=1,nElmtDofs
+                lodInte(this%m_localToGlobal(i)) = lodInte(this%m_localToGlobal(i)) + force(i)
+            enddo
+        enddo
+    end subroutine Segment_LoadInternal
+
+    subroutine Segment_get_angle_triad(triad_11,triad_22,tx,ty,tz)
+        implicit none
+        real(8):: triad_11(3,3),triad_22(3,3)
+        real(8):: rr(3,3)
+        real(8):: tx,ty,tz, dtx,dty,dtz,c1,tt,sint
+        integer:: i,j,k
+        !
+        ! get angle between two triads
+        do    i=1,3
+        do    j=1,3
+            rr(i,j)=0.0d0
+            do    k=1,3
+                rr(i,j)=rr(i,j) + triad_22(i,k)*triad_11(j,k)
+            enddo
+        enddo
+        enddo
+    
+        dtx = (rr(3,2)-rr(2,3))/2.0d0
+        dty = (rr(1,3)-rr(3,1))/2.0d0
+        dtz = (rr(2,1)-rr(1,2))/2.0d0
+    
+        c1=1.0d0
+        sint = dsqrt(dtx*dtx+dty*dty+dtz*dtz)
+    
+        if (sint .gt. 1.0d0) sint=1.0d0
+        tt = dasin(sint)
+        if ( sint .lt. 1.0d-6) then
+             c1=1.0d0
+        else
+             c1 = tt/sint
+        endif
+    
+        tx=c1*dtx
+        ty=c1*dty
+        tz=c1*dtz
+    
+        return
+    end subroutine Segment_get_angle_triad
+    
+    subroutine Segment_global_to_local(triad,tx,ty,tz,tx2,ty2,tz2)
+        implicit none
+        real(8):: triad(3,3)
+        real(8):: tx,ty,tz,tx2,ty2,tz2
+        tx2 = triad(1,1)*tx+triad(2,1)*ty+triad(3,1)*tz
+        ty2 = triad(1,2)*tx+triad(2,2)*ty+triad(3,2)*tz
+        tz2 = triad(1,3)*tx+triad(2,3)*ty+triad(3,3)*tz
+        return
+    end subroutine Segment_global_to_local
+
+    subroutine Segment_UpdateTriad_D(this)
+        implicit none
+        class(Segment), intent(inout) :: this
+        real(8):: rr(3,3)
+        real(8):: dtx1,dty1,dtz1
+        integer:: i
+        real(8) :: xElmtDofs(nElmtDofs),x(1:g_ndofs)
+
+        do  i=1,nElmtDofs
+            xElmtDofs(i)=x(this%m_localToGlobal(i))
+        enddo
+        ! n1 node
+        dtx1=xElmtDofs(4)
+        dty1=xElmtDofs(5)
+        dtz1=xElmtDofs(6)
+        call Segment_FiniteRot(dtx1,dty1,dtz1,rr)
+        this%triad_n1=matmul(rr,this%triad_n1)
+        ! n2 node
+        dtx1=xElmtDofs(10)
+        dty1=xElmtDofs(11)
+        dtz1=xElmtDofs(12)
+        call Segment_FiniteRot(dtx1,dty1,dtz1,rr)
+        this%triad_n2=matmul(rr,this%triad_n2)
+
+        return
+    end subroutine Segment_UpdateTriad_D
+
+    subroutine Segment_MakeTriad_ee(this)
+        implicit none
+        class(Segment), intent(inout) :: this
+        real(8):: triad_aa(3,3)
+        real(8):: rr(3,3),tx,ty,tz
+        real(8):: triad_11(3,3),triad_22(3,3)
+        !
+        real(8):: dx1,dy1,dz1
+        
+        real(8):: xll,xmm,xnn,dd,r2e1,r3e1
+        integer:: i,j,k
+        !
+        dx1   = this%x1(1) - this%x1(7)
+        dy1   = this%x1(2) - this%x1(8)
+        dz1   = this%x1(3) - this%x1(9)
+        xll=dx1/this%len1
+        xmm=dy1/this%len1
+        xnn=dz1/this%len1
+        dd =dsqrt(xll*xll+xmm*xmm)
+        do    i=1,3
+        do    j=1,3
+            this%triad_ee(i,j)=0.0d0
+        enddo
+        enddo
+        this%triad_ee(1,1)=xll
+        this%triad_ee(2,1)=xmm
+        this%triad_ee(3,1)=xnn
+        !
+        ! get angle between two triads
+        do    i=1,3
+        do    j=1,3
+            triad_11(i,j)=this%triad_n1(i,j)
+            triad_22(i,j)=this%triad_n2(i,j)
+        enddo
+        enddo
+        call Segment_get_angle_triad(triad_11,triad_22,tx,ty,tz)
+        !
+        ! rotate n1 to intermediate
+        tx=tx/2.0d0
+        ty=ty/2.0d0
+        tz=tz/2.0d0
+        call Segment_FiniteRot(tx,ty,tz,rr)
+        triad_aa(1:3,1:3)=matmul(rr(1:3,1:3),this%triad_n1(1:3,1:3))
+        !
+        ! vectors e2 e3
+        r2e1 = 0.0d0
+        r3e1 = 0.0d0
+        do    k=1,3
+            r2e1 = r2e1 + triad_aa(k,2)*this%triad_ee(k,1)
+            r3e1 = r3e1 + triad_aa(k,3)*this%triad_ee(k,1)
+        enddo
+        do    j=1,3
+            this%triad_ee(j,2)=triad_aa(j,2) - r2e1*(triad_aa(j,1)+this%triad_ee(j,1))/2.0d0
+            this%triad_ee(j,3)=triad_aa(j,3) - r3e1*(triad_aa(j,1)+this%triad_ee(j,1))/2.0d0
+        enddo
+        return
+    end subroutine Segment_MakeTriad_ee
+    
+    subroutine Segment_FiniteRot(t1,t2,t3,rr)
+        implicit none
+        real(8):: rr(3,3),rr1(3,3),rr2(3,3),rr3(3,3)
+        real(8):: t1,t2,t3,tt,ss,cc,c1,c2
+        !
+        integer:: i,j
+        !
+        tt=dsqrt( t1**2 + t2**2 + t3**2 )
+        ss=dsin(tt)
+        cc=dcos(tt)
+        !
+        rr1(1,1)=1.0d0
+        rr1(1,2)=0.0d0
+        rr1(1,3)=0.0d0
+        rr1(2,1)=0.0d0
+        rr1(2,2)=1.0d0
+        rr1(2,3)=0.0d0
+        rr1(3,1)=0.0d0
+        rr1(3,2)=0.0d0
+        rr1(3,3)=1.0d0
+        !
+        rr2(1,1)=0.0d0
+        rr2(1,2)=-t3
+        rr2(1,3)= t2
+        rr2(2,1)= t3
+        rr2(2,2)=0.0d0
+        rr2(2,3)=-t1
+        rr2(3,1)=-t2
+        rr2(3,2)= t1
+        rr2(3,3)=0d0
+        !
+        rr3(1,1)=-t3*t3-t2*t2
+        rr3(1,2)= t2*t1
+        rr3(1,3)= t3*t1
+        rr3(2,1)= t1*t2
+        rr3(2,2)=-t3*t3-t1*t1
+        rr3(2,3)= t3*t2
+        rr3(3,1)= t1*t3
+        rr3(3,2)= t2*t3
+        rr3(3,3)=-t2*t2-t1*t1
+        !
+        do    i=1,3
+        do    j=1,3
+            if    (tt .lt. 1.0d-10) then
+                c1=1.0d0
+                ! c2=1.0
+                c2=0.5d0
+            else
+                c1 = ss/tt
+                c2 = (1.0d0-cc)/tt**2
+            endif
+            rr(i,j) = rr1(i,j) + rr2(i,j)*c1 + rr3(i,j)*c2
+        enddo
+        enddo
+        !
+        return
+    end subroutine Segment_FiniteRot
 
 end module BeamStrucutre
 
